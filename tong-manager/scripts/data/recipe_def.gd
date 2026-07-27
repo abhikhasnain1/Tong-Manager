9f0b4f625a3df095ca53811538b15a426cf1e883
Abhik Hasnain <167350261+abhikhasnain1@users.noreply.github.com>
2026-07-26 18:34:31 -0600

    Create recipe definition resources
    Closes #35

---
+++ tong-manager/scripts/data/recipe_def.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+@export var id: StringName
+@export var display_name_key: String
+@export var base_item_ids: Array[StringName] = []
+@export var relish_item_ids: Array[StringName] = []
+@export var output_item_id: StringName
+@export var price: int
+@export var unlocked_by_default: bool
