ee222fb3ad1fd8cec8470898dd0bc3f88e04cf26
Abhik Hasnain <167350261+abhikhasnain1@users.noreply.github.com>
2026-07-26 18:22:15 -0600

    Create item definition resources
    Closes #34

---
+++ tong-manager/scripts/data/item_def.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+@export var id: StringName
+@export var display_name_key: String
+@export var category: StringName
+@export var icon: Texture2D
+@export var unit: StringName
+@export var stack_size: int
+@export var cost: int
+@export var sell_price: int
