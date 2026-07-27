bad6893122041cc8d5264881c9d0bda069c0e540
Abhik Hasnain <167350261+abhikhasnain1@users.noreply.github.com>
2026-07-26 18:44:13 -0600

    Create tool and day configuration resources
    Closes #36

---
+++ tong-manager/scripts/data/day_config.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+@export var start_hour: int
+@export var interaction_minutes: int
+@export var customer_count_min: int
+@export var customer_count_max: int
+@export var starting_cash: int
+@export var starting_inventory: Dictionary
---
+++ tong-manager/scripts/data/tool_def.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+@export var tray_capacity: int
+@export var bucket_capacity: int
+@export var kettle_capacity_ml: int
+@export var cup_volume_ml: int
+@export var burner_count: int
+@export var tray_grid_size: Vector2i
+@export var tray_table_footprint: Vector2i
